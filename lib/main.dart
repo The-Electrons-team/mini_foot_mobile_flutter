41ce82dff55ce5a8b6dc19e7b13ca557ade17b9f
Mamadou Sy <92mamadousy@gmail.com>
2026-08-31 14:35:50 +0200

    fix: start mobile app without web Firebase env

--- lib/main.dart
+++ lib/main.dart
@@ -24,20 +24,19 @@ void main() async {
   const envFileName = String.fromEnvironment('ENV_FILE', defaultValue: '.env');
   await dotenv.load(fileName: envFileName);
   
-  // Configuration Web lue depuis le .env
-  final firebaseOptions = FirebaseOptions(
-    apiKey: dotenv.get('FIREBASE_API_KEY'),
-    authDomain: dotenv.get('FIREBASE_AUTH_DOMAIN'),
-    projectId: dotenv.get('FIREBASE_PROJECT_ID'),
-    storageBucket: dotenv.get('FIREBASE_STORAGE_BUCKET'),
-    messagingSenderId: dotenv.get('FIREBASE_MESSAGING_SENDER_ID'),
-    appId: dotenv.get('FIREBASE_APP_ID'),
-    measurementId: dotenv.get('FIREBASE_MEASUREMENT_ID'),
-  );
-
   try {
     if (kIsWeb) {
-      await Firebase.initializeApp(options: firebaseOptions);
+      await Firebase.initializeApp(
+        options: FirebaseOptions(
+          apiKey: dotenv.get('FIREBASE_API_KEY'),
+          authDomain: dotenv.get('FIREBASE_AUTH_DOMAIN'),
+          projectId: dotenv.get('FIREBASE_PROJECT_ID'),
+          storageBucket: dotenv.get('FIREBASE_STORAGE_BUCKET'),
+          messagingSenderId: dotenv.get('FIREBASE_MESSAGING_SENDER_ID'),
+          appId: dotenv.get('FIREBASE_APP_ID'),
+          measurementId: dotenv.get('FIREBASE_MEASUREMENT_ID'),
+        ),
+      );
     } else {
       await Firebase.initializeApp();
     }
